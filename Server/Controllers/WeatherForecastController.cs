bf40434db373bb540c1885ea34c909ebe2164c02
Grace H <Grace@hypnovir.us>
2020-08-26 20:38:11 -0700

    Initial commit.

---
+++ Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BuzzOff.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return Enumerable.Range(1, 69);
+        }
+    }
+}
---
+++ Server/Hubs/BuzzHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuzzOff.Server.Hubs
+{
+    public class BuzzHub : Hub
+    {
+        public async Task BuzzIn(string user)
+        {
+            await Clients.All.SendAsync("BuzzedIn", user);
+        }
+    }
+}
