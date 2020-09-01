61f7d5f3a0d8032ad5b99f9c24f7705a604587c9
Grace H <grace@hypnovir.us>
2020-09-01 15:22:41 -0700

    See if this properly shows the date and commit stamp.

---
+++ Server/BuildInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuzzOff.Server
+{
+	public static class BuildInfo
+	{
+		public const string GitHash = "@@GIT_HASH@@";
+		public const string BuildTimestamp = "@@BUILD_TIME@@";
+	}
+}
