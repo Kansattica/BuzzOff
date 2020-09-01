db9c23342906ab826f1fb1d0f4828cceebb0999b
Grace H <Grace@hypnovir.us>
2020-08-31 17:46:17 -0700

    Super bare-bones landing page is done.

---
+++ Server/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿const output = document.getElementById("output");
+const roomname = document.getElementById("roomname");
+const baseUrl = new URL('/', location.href).href;
+if (!baseUrl.endsWith('/'))
+    baseUrl += '/';
+
+roomname.oninput = () => {
+    output.innerText = output.href = encodeURI(baseUrl + "Room/" + roomname.value);
+}
