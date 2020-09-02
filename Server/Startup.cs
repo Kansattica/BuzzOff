0e11843ff9d45a30e523f36851e8f1487a8be89d
Grace H <Grace@hypnovir.us>
2020-09-02 12:16:09 -0700

    Only do HTTPS in production mode.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -62,9 +62,8 @@ namespace BuzzOff.Server
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
-
-            app.UseHttpsRedirection();
 
             var provider = new FileExtensionContentTypeProvider();
             provider.Mappings[".webmanifest"] = "application/manifest+json";
