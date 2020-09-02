26890ec0e1561b44eb5753f4ba68345af9e97762
Grace H <grace@hypnovir.us>
2020-09-02 13:38:34 -0700

    Rewrite the landing page to use the same layout as everyone else.

--- Server/Pages/Room.cshtml.cs
+++ Server/Pages/Room.cshtml.cs
@@ -15,6 +15,9 @@ namespace BuzzOff.Server.Pages
 		public string RoomId { get; set; }
 		public string UserName { get; set; }
 
+		private static readonly string[] _javascriptPaths = new string[] { "~/js/dist/browser/signalr.min.js", "~/js/buzz.js" };
+		public string[] JavascriptPaths => _javascriptPaths;
+
 		public void OnGet()
 		{
 			if (Request.Query.TryGetValue("user", out var value))
