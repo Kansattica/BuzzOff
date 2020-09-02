6005a763f599f0158f0e1e13c349332ed81dd1f3
Grace H <grace@hypnovir.us>
2020-09-02 13:19:49 -0700

    Make editorconfig happy.

--- Server/Entities/Room.cs
+++ Server/Entities/Room.cs
@@ -10,30 +10,14 @@ namespace BuzzOff.Server.Entities
         public User RoomHost { get; set; }
         public List<User> Users { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as Room);
-        }
+		public override bool Equals(object obj) => Equals(obj as Room);
 
-        public bool Equals(Room other)
-        {
-            return other != null &&
-                   SignalRId == other.SignalRId;
-        }
+		public bool Equals(Room other) => other != null && SignalRId == other.SignalRId;
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(SignalRId);
-        }
+		public override int GetHashCode() => HashCode.Combine(SignalRId);
 
-        public static bool operator ==(Room left, Room right)
-        {
-            return EqualityComparer<Room>.Default.Equals(left, right);
-        }
+		public static bool operator ==(Room left, Room right) => EqualityComparer<Room>.Default.Equals(left, right);
 
-        public static bool operator !=(Room left, Room right)
-        {
-            return !(left == right);
-        }
-    }
+		public static bool operator !=(Room left, Room right) => !(left == right);
+	}
 }
