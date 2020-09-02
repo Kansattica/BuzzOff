a494f1191d110ebc911ce018556546f95ed6b3b1
Grace H <Grace@hypnovir.us>
2020-09-02 12:17:22 -0700

    We're not using the default value for HSTS.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -60,7 +60,6 @@ namespace BuzzOff.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
             }
