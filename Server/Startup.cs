b417fe7b47aa7e761a01b44b9c27202b42764ab5
Grace Lovelace <grace@hypnovir.us>
2020-11-13 13:40:14 -0800

    Update NuGet packages while we're at it.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -44,6 +44,7 @@ namespace BuzzOff.Server
 			});
 
 			services.AddApplicationInsightsTelemetry();
+			services.AddCompressedStaticFiles(options => options.RemoveImageSubstitutionCostRatio());
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
